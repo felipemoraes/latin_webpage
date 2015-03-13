e794bb82fec27a75b024350760a2d2f110607f7b
Felipe Moraes <felipemoraes@Felipes-MacBook-Air.local>
2015-03-13 19:37:36 -0300

    Scaffolded with yeoman.io and put Scrolling Nav template

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name latinHomepageApp
+ * @description
+ * # latinHomepageApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('latinHomepageApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name latinHomepageApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the latinHomepageApp
+ */
+angular.module('latinHomepageApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
